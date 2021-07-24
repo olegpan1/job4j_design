a6ffaed050cdd221424efc584079f8ba67348d46
olegpan1 <olegpan@hotmail.com>
2021-07-24 15:04:29 +0700

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/map/UserWithoutEqualsHashCode.java
+++ src/main/java/ru/job4j/collection/map/UserWithoutEqualsHashCode.java
@@ -14,11 +14,14 @@ public class UserWithoutEqualsHashCode {
         User user1 = new User("Ivan", 2, BIRTHDAY);
         User user2 = new User("Ivan", 2, BIRTHDAY);
 
-        System.out.println(user1.hashCode());
-        System.out.println(user2.hashCode());
+        System.out.println("User1 hashcode: " + user1.hashCode());
+        System.out.println("User2 hashcode: " + user2.hashCode());
 
         System.out.println(usersMap.put(user1, 1));
-        System.out.println(usersMap.put(user2, 2));
+        System.out.println(usersMap.put(user2, 1));
+
+        System.out.println("User1 hash: " + (user1.hashCode() >>> 16));
+        System.out.println("User2 hash: " + (user2.hashCode() >>> 16));
 
         usersMap.forEach((key, value) -> System.out.println("Key : " + key + "  Value : " + value));
     }

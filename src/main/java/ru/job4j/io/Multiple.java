8ae134a3c218f7367a930975d37da912dd429f56
olegpan1 <olegpan@hotmail.com>
2021-08-02 07:52:14 +0700

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Multiple {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+            out.write("1 * 1 = 1  ".getBytes());
+            out.write("1 * 2 = 2  ".getBytes());
+            out.write("1 * 3 = 3  ".getBytes());
+            out.write("1 * 4 = 4  ".getBytes());
+            out.write("1 * 5 = 5  ".getBytes());
+            out.write("1 * 6 = 6  ".getBytes());
+            out.write("1 * 7 = 7  ".getBytes());
+            out.write("1 * 8 = 8  ".getBytes());
+            out.write("1 * 9 = 9  ".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

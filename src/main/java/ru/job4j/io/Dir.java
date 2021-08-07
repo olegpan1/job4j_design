c0a0ce3711075bde6903348e9b2cc980e0cff84b
olegpan1 <olegpan@hotmail.com>
2021-08-07 18:15:11 +0700

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -12,8 +15,8 @@ public class Dir {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
         System.out.printf("size : %s%n", file.getTotalSpace());
-        for (File subFile : file.listFiles()) {
-            System.out.println(subFile.getName() + " : " + subFile.length());
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getName());
         }
     }
 }

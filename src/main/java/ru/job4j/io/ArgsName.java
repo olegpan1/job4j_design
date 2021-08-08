255210d044d5cf916e6ba065cf66e19b738814a8
olegpan1 <olegpan@hotmail.com>
2021-08-08 09:01:34 +0700

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Arguments not found");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String str : args) {
+            if (!str.startsWith("-") || !str.contains("=") || str.endsWith("=")) {
+                throw new IllegalArgumentException("Bad arguments(" + str + "). Usage java -parameter=value");
+            }
+            String[] keyValue = str.substring(1).split("=");
+            values.put(keyValue[0], keyValue[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}

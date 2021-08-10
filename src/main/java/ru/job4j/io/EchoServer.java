5ac7f8e67a29a30481341542f62f7638aeaec7f1
olegpan1 <olegpan@hotmail.com>
2021-08-10 08:54:34 +0700

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,7 +11,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 System.out.println("Server open");
@@ -37,6 +43,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
         }
     }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,17 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Petr Arsentev";
-        int age = 33;
-        byte children = 2;
-        short weight = 70;
-        long height = 170;
-        float shoesSize = 42.5F;
-        double salary = 1000000.3;
-        char ch = 'a';
-        boolean atWork = true;
-        LOG.debug("User info name : {}, age : {}, children : {}, weight : {}, "
-                + "height : {}, shoesSize : {}, salary : {}, ch : {}, atWork : {}",
-                name, age, children, weight, height, shoesSize, salary, ch, atWork);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }

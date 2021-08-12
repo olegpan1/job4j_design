73b017f635d0f721a1e6a3d22f71927e06fa22d7
olegpan1 <olegpan@hotmail.com>
2021-08-12 16:39:26 +0700

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Car.java
+++ src/main/java/ru/job4j/serialization/json/Car.java
@@ -1,7 +1,12 @@
 package ru.job4j.serialization.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @XmlRootElement(name = "car")
@@ -26,6 +31,55 @@ public class Car {
         this.dateOfManufacture = dateOfManufacture;
         this.contactDealer = contactDealer;
         this.description = description;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public int getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public Contact getContactDealer() {
+        return contactDealer;
+    }
+
+    public String[] getDescription() {
+        return description;
+    }
+
+    public static void main(String[] args) {
+        final Car car = new Car("Porsche 911", true, 2010,
+                new Contact("0-123-456"), "Red Color", "Cabriolet");
+
+        /* JSONObject из json-строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"0-123-456\"}");
+        System.out.println(jsonContact);
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add(car.getDescription()[0]);
+        list.add("Cabriolet");
+        JSONArray jsonDescription = new JSONArray(list);
+        System.out.println(jsonDescription.toString());
+
+        /* JSONObject напрямую методом put */
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("inStock", car.isInStock());
+        jsonObject.put("dateOfManufacture", car.getDateOfManufacture());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("description", jsonDescription);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект car в json-строку */
+        System.out.println(new JSONObject(car).toString());
     }
 
     @Override

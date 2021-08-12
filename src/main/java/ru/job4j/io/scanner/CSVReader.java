0eb65b165f0c3865bf4669f1499731fdb0ae120e
olegpan1 <olegpan@hotmail.com>
2021-08-12 13:10:56 +0700

    7. Scanner [#504791 #187797]

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -4,66 +4,83 @@ import ru.job4j.io.ArgsName;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CSVReader {
-    List<InfoCSV> list = new ArrayList<>();
+    List<String> list = new ArrayList<>();
     String[] title;
+    List<Integer> filterColumn = new ArrayList<>();
     StringBuilder filterResult = new StringBuilder();
 
     public void manageCSV(Path sources, String delimiter, String target, String[] filter) throws FileNotFoundException {
-        // Считывание из CSV
-        readCSV(sources, delimiter);
-        //Фильтрация данных
-        filterCSV(delimiter, filter);
-        // Запись результата в CSV
-        writeCSV(filterResult, target);
+        // Считывание из CSV файла и фильтрация данных
+        readCSV(sources, delimiter, filter);
+        //Подготовка данных для вывода
+        resultCSV(delimiter);
+        // Вывод результата в формате CSV
+        writeCSV(target);
     }
 
-    public void readCSV(Path sources, String delimiter) throws FileNotFoundException {
+    public void readCSV(Path sources, String delimiter, String[] filter) throws FileNotFoundException {
         try (var scanner = new Scanner(new BufferedInputStream(new FileInputStream(sources.toString())))
                 .useDelimiter(System.lineSeparator())) {
-            // Считывание названий колонок
+            // Считывание названий колонок,проверка делимитра и  фильтра, определение номера колонок для вывода
             if (scanner.hasNext()) {
-                title = scanner.next().split(delimiter);
+                String delimiterCorrect = scanner.next();
+                if (!delimiterCorrect.contains(delimiter)) {
+                    throw new IllegalArgumentException("Delimiter: " + delimiter
+                            + " is wrong! Or filename in parameter -path= is wrong ");
+                }
+                title = delimiterCorrect.split(delimiter);
+                for (String f : filter) {
+                    boolean filterCorrect = false;
+                    for (int i = 0; i < title.length; i++) {
+                        if (f.equals(title[i])) {
+                            filterColumn.add(i);
+                            list.add(f);
+                            filterCorrect = true;
+                            break;
+                        }
+                    }
+                    if (!filterCorrect) {
+                        for (String s : title) {
+                            filterResult.append(s).append(", ");
+                        }
+                        throw new IllegalArgumentException("Filter: \"" + f + "\" is wrong! "
+                                + "Correct filters: " + filterResult.toString());
+                    }
+                }
             }
-            // Считывание данных
+            // Считывание  и сохранение отфильтрованных данных
             while (scanner.hasNext()) {
                 String[] scArray = scanner.next().split(delimiter);
-                Map<String, String> line = new HashMap<>();
-                int i = 0;
-                for (String t : title) {
-                    line.put(t, scArray[i++]);
+                for (Integer i : filterColumn) {
+                    list.add(scArray[i]);
                 }
-                list.add(new InfoCSV(line));
             }
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Please provide the correct name of the source file in parameter  -path");
-//            e.printStackTrace();
         }
     }
 
-    public void writeCSV(StringBuilder rsl, String target) {
+    public void writeCSV(String target) {
         try (BufferedOutputStream out = new BufferedOutputStream((target.equals("stdout"))
                 ? new PrintStream(System.out) : new PrintStream(target))) {
-            out.write(rsl.toString().getBytes());
+            out.write(filterResult.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //Фильтрация данных
-    public void filterCSV(String delimiter, String[] filter) {
-        // Добавление заголовков фильтра
-        for (String filterTitle : filter) {
-            filterResult.append(filterTitle).append(delimiter);
-        }
-        filterResult.append(System.lineSeparator());
+    //подготовка данных для выаода
+    public void resultCSV(String delimiter) {
 
-        // Добавление результата отбора по фильтру
-        for (InfoCSV info : list) {
-            for (String s : filter) {
-                filterResult.append(info.getMap().get(s)).append(delimiter);
+        int i = 0;
+        while (i < list.size()) {
+            int x = 0;
+            while (x < filterColumn.size()) {
+                filterResult.append(list.get(i++)).append(delimiter);
+                x++;
             }
             filterResult.append(System.lineSeparator());
         }

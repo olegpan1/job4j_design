e86333d78fb92e212af945434a00eac19f8ffd55
olegpan1 <olegpan@hotmail.com>
2021-08-25 08:16:19 +0700

    Хранилище машин [#1733 #193045]

--- src/main/java/ru/job4j/sql/cars_storage.sql
+++ src/main/java/ru/job4j/sql/cars_storage.sql
@@ -16,9 +16,9 @@ create table gearboxes(
 create table cars(
     id serial primary key,
     name varchar(50),
-    body_id int references bodys,
-    engine_id int references engines,
-    gearbox_id int references gearboxes
+    body_id int references bodys(id),
+    engine_id int references engines(id),
+    gearbox_id int references gearboxes(id)
 );
 
 insert into bodys(name) values ('Body 1'), ('Body 2'), ('Body 3'), ('Body 4');
@@ -27,12 +27,12 @@ insert into engines(name) values ('Engine 1'), ('Engine 2'), ('Engine 3'), ('Eng
 
 insert into gearboxes(name) values ('Gearbox 1'), ('Gearbox 2'), ('Gearbox 3'), ('Gearbox 4');
 
-insert into cars(name, body_id, engine_id, gearbox_id) values  ('Car 1', 1, 1, 1), ('Car 2', 2, 2, 2);
+insert into cars(name, body_id, engine_id, gearbox_id) values  ('Car 1', 1, 1, 1), ('Car 2', 1, 2, 3);
 
 select * from cars;
 
-select * from bodys as b left join cars c on b.id = c.body_id where c.id is null limit 1;
+select * from bodys as b left join cars c on b.id = c.body_id where c.id is null;
 
-select * from engines as e left join cars c on e.id = c.body_id where c.id is null limit 1;
+select * from engines as e left join cars c on e.id = c.engine_id where c.id is null;
 
-select * from gearboxes as g left join cars c on g.id = c.body_id where c.id is null limit 1;
+select * from gearboxes as g left join cars c on g.id = c.gearbox_id where c.id is null;

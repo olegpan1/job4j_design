f43d452268d72537f6057e250feb9391d3b054b3
olegpan1 <olegpan@hotmail.com>
2022-07-22 11:50:43 +0700

    0.2. FileInputStream [#4898 #185142]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,18 +1,14 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
+            try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
+                in.lines().forEach(System.out::println);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println(text);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
-}

819a436ea3767bb18427bb754662d474fa911218
olegpan1 <olegpan@hotmail.com>
2022-08-19 15:25:50 +0700

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/LibraryGeneratorTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LibraryGeneratorTest {
+    @Test
+    public void whenRight() {
+        Generator generator = new LibraryGenerator();
+        Map<String, String> keys = new HashMap<>();
+        keys.put("Petr Arsentev", "you");
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        assertThat(expected).isEqualTo(generator.produce("I am a ${name}, Who are ${subject}? ", keys));
+    }
+    @Test
+    public void whenExtraKey() {
+        Generator generator = new LibraryGenerator();
+        Map<String, String> keys = new HashMap<>();
+        keys.put("Petr Arsentev", "you");
+        assertThrows(IllegalArgumentException.class,
+                () -> generator.produce("I am a ${name}, I live in %{city}, Who are ${subject}? ", keys));
+    }
+    @Test
+    public void whenNotEnoughKey() {
+        Generator generator = new LibraryGenerator();
+        Map<String, String> keys = new HashMap<>();
+        keys.put("Petr Arsentev", "you");
+        assertThrows(IllegalArgumentException.class,
+                () -> generator.produce("I am a ${name}, Hello! ", keys));
+    }
+}

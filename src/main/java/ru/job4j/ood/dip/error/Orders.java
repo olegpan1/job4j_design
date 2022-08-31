28124eddaef2e9cdc1da7bb83b52c2caaf7b8978
olegpan1 <olegpan@hotmail.com>
2022-08-31 10:01:16 +0700

    0. Принцип инверсии зависимостей [#4917]

---
+++ src/main/java/ru/job4j/ood/dip/error/Orders.java
@@ -0,0 +1,32 @@
+package ru.job4j.ood.dip.error;
+
+import java.util.ArrayList;
+
+
+public class Orders {
+    /**
+     * Ошибка в зависимости от конкретной реализации, а не от абстракции
+     * Нужно использовать абстракцию List
+     */
+    private ArrayList<String> orders;
+
+
+    /**
+     * Соответственно ошибка в параметре, тоже нужно использовать абстракцию, а не конкретную реализацию
+     * Нужно использовать абстракцию List
+     */
+    public Orders(ArrayList orders) {
+        this.orders = orders;
+    }
+
+    /**
+     * Ошибка в зависимости от конкретного способа печати заказа.
+     * Нужно сделать отдельный интерфейс Print от которого можно будет реализовывать разные способы вывода информации
+     * о заказе
+     */
+
+    public void print(Orders orders) {
+        System.out.println(orders);
+    }
+
+}
